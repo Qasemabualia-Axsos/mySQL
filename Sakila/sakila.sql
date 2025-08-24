b854da9f56cc52a4d24092a789b2aa14118324d2
Qasem Ehab kamel Aboalia <qassimalia77@gmail.com>
2025-08-24 16:42:48 +0300

    Add files via upload

---
+++ Sakila/sakila.sql
@@ -0,0 +1,70 @@
+use sakila;
+-- question1:
+select customer.first_name, customer.last_name, customer.email, address.address
+from address
+join customer
+ on customer.address_id = address.address_id
+where address.city_id = 312;
+
+-- question2:
+select film.title,film.description,film.release_year,film.rating,film.special_features,category.name
+from film
+join film_category
+on film.film_id=film_category.film_id
+join category
+on category.category_id=film_category.category_id
+where category.name="comedy";
+ 
+ 
+-- question3:
+Select actor.first_name, actor.actor_id, film.title, film.description, film.release_year 
+from  film 
+join film_actor
+ on film.film_id = film_actor.film_id
+join actor
+ on actor.actor_id = film_actor.actor_id
+where  actor.actor_id="5";
+
+
+-- question4:
+select customer.first_name,customer.last_name,customer.email,address.address
+from address
+join customer
+on customer.address_id = address.address_id
+join store
+on customer.store_id=store.store_id
+
+where customer.store_id = 1 ;
+
+-- question5:
+select film.title , film.description ,film.release_year, film.rating , film.special_features 
+from film
+where film.rating="G"
+ and film.special_features like '%Behind the Scenes%' ;
+
+-- question6:
+select film.title, film.film_id   ,actor.actor_id , actor.first_name 
+from film
+join film_actor on film.film_id = film_actor.film_id
+join actor on actor.actor_id = film_actor.actor_id
+where   film.film_id = 369;
+
+-- question7:
+ select film.film_id,film.title, film.description ,film.release_year, film.rating , film.special_features,film.rental_rate ,category.name
+ from film
+ join film_category on film.film_id = film_category.film_id
+join category on category.category_id = film_category.category_id
+where category.name = "comedy" and rental_rate = 2.99;
+
+-- question8:
+ select film.film_id,film.title, film.description ,film.release_year, film.rating , film.special_features,category.name, actor.first_name,actor.last_name
+ from film
+ join film_category on film.film_id = film_category.film_id
+join category on category.category_id = film_category.category_id
+join film_actor on film.film_id = film_actor.film_id
+join actor on actor.actor_id = film_actor.actor_id
+where actor.first_name = "sandra" and actor.last_name = "KILMER";
+
+
+
+ 
